dab5575684d15c7a02c7796e5d2b210e19ada840
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-01-22 15:55:53 -0600

    update

--- src/component/Users.js
+++ src/component/Users.js
@@ -1,7 +1,23 @@
-import {Box ,Table,TableBody,TableCell,TableHead,TableRow,Typography,styled} from "@mui/material"
+
+
+import {Box ,Table,TableBody,TableCell,TableHead,TableRow,Typography,styled ,Button} from "@mui/material"
 const Component = styled(Box)`
 width:80%;
 margin:50px auto;
+& > h4{
+  margin-bottom:20px;
+}
+& > div> table > thead{
+  background-color:#000;
+  font-weight: 600;
+}
+& > div> table > thead >tr >th{
+  color:#fff;
+  font-size:16px;
+}
+& > div> table > tbody >tr >th{
+  font-size:16px;
+}
 `
 const Users = () => {
   return (
@@ -26,7 +42,7 @@ const Users = () => {
               <TableCell>example.@gmail.com</TableCell>
               <TableCell>20238</TableCell>
               <TableCell>24</TableCell>
-              <TableCell>Remove</TableCell>
+              <TableCell><Button variant="contained" color="error">Remove</Button></TableCell>
            </TableRow>
           </TableBody>
         </Table>
