0e475431e04ea89440917e065edcd232cee204af
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-01-23 18:16:20 -0600

    update

--- src/component/Users.js
+++ src/component/Users.js
@@ -1,4 +1,4 @@
-
+import {useState} from 'react'
 
 import {Box ,Table,TableBody,TableCell,TableHead,TableRow,Typography,styled ,Button} from "@mui/material"
 const Component = styled(Box)`
@@ -19,7 +19,27 @@ margin:50px auto;
   font-size:16px;
 }
 `
+const defaulObject = [
+  {
+    id:101,
+    name:'baray',
+    email:'baray@gmail.com',
+    phone:1245655111,
+    salary:45879,
+    age:40,
+    
+  
+  
+  }
+]
 const Users = () => {
+  const [users ,setUsers] = useState(defaulObject);
+
+  const removeEntry =(id)=>{
+    const updateUsers = users.filter(item => item.id !== id)
+   setUsers(updateUsers)
+  }
+
   return (
     <Component>
       <Typography variant="h4">Users</Typography>
@@ -36,14 +56,19 @@ const Users = () => {
            </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>101</TableCell>
-              <TableCell>Code for Interview</TableCell>
-              <TableCell>example.@gmail.com</TableCell>
-              <TableCell>20238</TableCell>
-              <TableCell>24</TableCell>
-              <TableCell><Button variant="contained" color="error">Remove</Button></TableCell>
+          { 
+           users.map((user,i) =>(
+              <TableRow key={i}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.salary}</TableCell>
+              <TableCell>{user.age}</TableCell>
+              <TableCell><Button variant="contained" color="error" onClick={()=>removeEntry(user.id)}>Remove</Button></TableCell>
            </TableRow>
+           ))
+          }
+             
           </TableBody>
         </Table>
       </Box>
